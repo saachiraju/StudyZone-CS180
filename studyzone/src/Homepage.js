0e45c012c4b3f57eafca2b74ad6a84303ae3a6b5
Akanksh Suhas Divyananda <adivy001@ucr.edu>
2025-05-05 20:32:33 -0700

    basic homepage

---
+++ studyzone/src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="homepage-container">
+      <h1>Welcome to StudyZone</h1>
+      <button className="login-button">Log In</button>
+    </div>
+  );
+}
+
+export default Homepage; 
