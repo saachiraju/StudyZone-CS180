18baa9d3f68b6221b1011b223c0112ee2c9caef8
Akanksh Suhas Divyananda <adivy001@ucr.edu>
2025-05-06 18:11:57 -0700

    Implement navigation structure and routing

--- studyzone/src/App.js
+++ studyzone/src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage';
+import Nav from './Nav';
 import { AuthProvider } from './AuthContext';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <AuthProvider>
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route 
+            path="/nav" 
+            element={
+              <ProtectedRoute>
+                <Nav />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
