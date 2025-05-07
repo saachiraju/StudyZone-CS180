b26754f971cb0e5b5d2ecaa4ef858b0c4350e0ad
Trisha Jain <trishajain12@ucr-secure-45-10-13-231-126.wnet.ucr.edu>
2025-05-07 14:54:09 -0700

    help

--- studyzone/src/pages/BCOE.js
+++ studyzone/src/pages/BCOE.js
@@ -1,29 +1,16 @@
-
-BCOE.js
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Pages.css';  
+import '../styles/Pages.css';
 
-export default function BCOE() {
+function BCOE() {
   return (
-    <div className="page-center">
-
-      <div className="card">
-        <h1 className="page-title">Bourns College of Engineering</h1>
-
+    <div className="page-container">
+      <h1>Bourns College of Engineering</h1>
+      <div className="page-content">
         <p>Welcome to the BCOE study resources page.</p>
         <p>Here you'll find resources for engineering students.</p>
-
-        <h2>Classes</h2>
-        <ul className="class-list">
-          <li>
-            <Link to="/bcoe/cs180">CS180 – Software Engineering</Link>
-          </li>
-          <li>
-            <Link to="/bcoe/cs153">CS153 – Operating Systems</Link>
-          </li>
-        </ul>
       </div>
     </div>
   );
 }
+
+export default BCOE; 
