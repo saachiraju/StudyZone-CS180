97758f37cc77bc84cca422167df585f6fbb92526
xuppii <98811769+xuppii@users.noreply.github.com>
2025-05-31 19:17:09 -0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from '@/dependencies/AuthContext';
+import {twMerge} from "tailwind-merge";
+import { DM_Sans } from "next/font/google";
 
 export const metadata = {
   title: 'Vercel Blob Starter',
   description: 'A simple Next.js app with Vercel Blob for image uploads',
 }
-
+const dmSans = DM_Sans({ subsets: ["latin"] });
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
@@ -20,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         <AuthProvider>{children}</AuthProvider>
         </body>
     </html>
